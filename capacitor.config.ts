c83d577b44f309a37a702007067c1e7ba33d0d14
daftclaud <cdsmileyone@gmail.com>
2022-03-06 10:43:46 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'friend-finder-ar',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
